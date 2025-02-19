5db934a6d0be67d9d55f6049b3baf0d243362d80
nath-asha <omthasha@gmail.com>
2025-02-19 12:57:48 +0530

    mongo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const bodyParser = require("body-parser");
 const data = require('../front/frontend/public/scores.json');
 
@@ -22,13 +23,20 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
         });
         console.log("MongoDB connected");
     } catch (error) {
         console.error("MongoDB connection error");
     }
 };
+
+connectDB();
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(() => console.log("MongoDB connected"))
+//   .catch(err => console.log(err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
