c072e72a374760400368b1de699fa348f68b187d
nath-asha <omthasha@gmail.com>
2025-02-28 14:12:05 +0530

    add new challenges and implement footer component

---
+++ front/frontend/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+const footer = () => {
+    return(
+        <footer>
+            <p>Footer content goes here</p>
+        </footer>
+    );
+};
+
+export default footer;
