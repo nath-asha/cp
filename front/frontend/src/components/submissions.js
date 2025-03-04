203e23d081fa655b72e7215ee5e6ca730c48e7ad
nath-asha <omthasha@gmail.com>
2025-03-04 10:47:11 +0530

    new submissions

--- front/frontend/src/components/submissions.js
+++ front/frontend/src/components/submissions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState,useEffect } from 'react';
+import Form from 'react-bootstrap/Form'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
 
@@ -11,7 +12,45 @@ const Submissions = () => {
     })
     return (
         <div className="container mt-5">
-            <h1 className="text-center mb-4" >Project Submissions</h1>
+            <h3>Project Title</h3>
+            <Form.Control type="text" placeholder='title' />
+            <br />
+            <h3>Github link</h3>
+            <Form.Control type="text" placeholder='github.com/username'/>
+            <br />
+            <h3>Built with?(Description)</h3>
+            <Form.Group className="mb-3">
+            <Form.Control as="textarea" rows={3} placeholder='describe your project what it does and how it helps or anything you would like us to know' />
+            </Form.Group>
+
+            <h3>Select Problem Statement</h3>
+            <Form.Select>
+                <option>select problem statement</option>
+                <option value='1'>ps 1</option>
+                <option value='2'>ps 2</option>
+                <option value='3'>ps 3</option>
+                <option value='4'>ps 4</option>
+            </Form.Select>
+            <h3>PPT /Link Upload</h3>
+            <Form.Control type="text" placeholder='drive link here ps:change permissions to editor'/>
+            <br />
+            <h3>Report Upload</h3>
+            <Form.Group controlId="formFile" className="mb-3">
+            <Form.Control type="file" />
+            </Form.Group>
+
+            <h3>Document link</h3>
+            <Form.Control type="text" placeholder='drive link here ps:change permissions to editor'/>
+            <br />
+            <h3>Video Link</h3>
+            <Form.Control type="text" placeholder='drive link here ps:change permissions to editor'/>
+            <br />
+
+            <button>Save and Continue</button>
+            <br></br>
+            <button>Cancel</button>
+            
+            {/* <h1 className="text-center mb-4" >Project Submissions</h1>
             <div className="card mb-3">
                 <div className="card-header">
                     <h2>Project Title</h2>
@@ -32,10 +71,10 @@ const Submissions = () => {
                     </ul>
                 </div>
                 <div className="card-footer text-right">
-                    {/* <button className="btn btn-primary mr-2">Add Participant</button> */}
+                    <button className="btn btn-primary mr-2">Add Participant</button>
                     <button className="btn btn-secondary">Manage Submission</button>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
