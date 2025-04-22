6a93124384763a1813fed07f474e0efb5ac53ad2
nath-asha <omna0504@gmail.com>
2025-04-22 11:14:26 +0530

    Add Signup model and Newsignup component; enhance Submissionlist layout with additional details

---
+++ front/frontend/src/components/newsignup.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+import { Form,Container,Row,Col } from "react-bootstrap";
+
+const Newsignup = () => {
+ const [formData, setFormData] = useState({
+    
+ })
+return(
+
+)};
+
+export default Newsignup;
--- front/frontend/src/components/submissionlist.js
+++ front/frontend/src/components/submissionlist.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
-import { Card,CardBody } from "react-bootstrap";
-import { Github } from "lucide-react";
+import { Card,CardBody,Row } from "react-bootstrap";
+import { Github, Video } from "lucide-react";
 
 const Submissionlist = () => {
     const [submissions, setSubmissions] = useState([]);
@@ -14,18 +14,21 @@ const Submissionlist = () => {
 return(
     <div>
                 <h2 className="text-black">Submissions</h2>
+                <Row xs={1} md={2} lg={3} className="g-4">
                         {submissions.map((submission, index) => (
                             <Card key={submission._id}>
                                 <CardBody>
-                                    <h5 className="card-title">Submission ID: {submission._id}</h5>
-                                    <p className="card-text">Team ID: {submission.team_id}</p>
+                                    <h5 className="card-title">Team ID: {submission.team_id}</h5>
+                                    <h5 className="card-title">Title: {submission.title}</h5>
                                     <a className="card-text" href={submission.gitrepo}><Github/></a>
+                                    <a className="card-text" href={submission.vid}><Video /></a>
                                     {/* yet to add submission date */}
                                     <p className="card-text">Submission Date: {new Date(submission.submission_date).toLocaleDateString()}</p>
                                     <p className="card-text">Status: {submission.status}</p>
                                 </CardBody>
                             </Card>
                         ))}
+                        </Row>
     </div>
                   
 )};
